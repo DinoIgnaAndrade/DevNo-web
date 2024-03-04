302812cc5b8147d8249883c6eae153383d31d3d5
DinoIgnaAndrade <127141499+DinoIgnaAndrade@users.noreply.github.com>
2024-03-04 19:06:56 -0300

    fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,27 +3,28 @@ import React, { useEffect, useState } from 'react';
 import styles from './Header.module.css';
 import Weather from '../Weather/Weather';
 
-const Header:React.FC = () => {
+const Header: React.FC = () => {
 
   return (
-      <header className={styles.container}>
+    <header className={styles.container}>
 
-          <div className={styles.presentation}>
+      <div className={styles.presentation}>
 
-            <h4>Programando por un sueño</h4>
-            <h1>Hola soy Dino, Web y Aplication developer</h1>
+        <h4>Programando...</h4>
+        <h1>Hola soy Dino, desarrollador Front-End web y mobile</h1>
 
-            <p className={styles.paragraph}>
-              Soy un apasionado desarrollador novato en el mundo de la creación de sitios web y aplicaciones.
-              A pesar de ser nuevo en este campo, he estado dedicando tiempo y esfuerzo para aprender; y crecer en mis
-              habilidades.
-            </p>
+        <p className={styles.paragraph}>
+          Soy un entusiasta desarrollador novato, explorando el fascinante mundo de la creación de sitios web y aplicaciones.
+          A pesar de ser nuevo en este emocionante campo, he invertido tiempo y esfuerzo para ampliar mis habilidades.
+          Mi enfoque se centra en el aprendizaje constante y la práctica activa. Creo firmemente en la importancia de construir
+          proyectos prácticos para aplicar los conocimientos adquiridos y desafiar mis límites.
+        </p>
 
-          </div>
+      </div>
 
-          <Weather />
-          
-      </header>
+      <Weather />
+
+    </header>
   )
 }
 
