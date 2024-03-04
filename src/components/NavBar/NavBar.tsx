66f0a58457ee5e7774f612cc2bdedd73bf6ce504
DinoIgnaAndrade <127141499+DinoIgnaAndrade@users.noreply.github.com>
2024-03-04 20:33:27 -0300

    feat: agregado de curriculum

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,32 +7,37 @@ import styles from "./NavBar.module.css";
 
 const NavBar: React.FC = () => {
 
+  const descargarPDF = () => {
+    const url = "../../img/Curriculum.pdf";
+    window.open(url, "_blank");
+  };
+
   return (
-      <nav className={styles.container}>
+    <nav className={styles.container}>
 
-        <Link  href='/'>
-          <Image
-            className={styles.imageLogo}
-            src={logo}
-            alt="Logo"
-            width={300}
-            height={300}
-          />
-        </Link>
-        <Link href='/'
-          className={styles.nameLogo}>
-          DevNo
-        </Link>
+      <Link href='/'>
+        <Image
+          className={styles.imageLogo}
+          src={logo}
+          alt="Logo"
+          width={300}
+          height={300}
+        />
+      </Link>
+      <Link href='/'
+        className={styles.nameLogo}>
+        DevNo
+      </Link>
 
-        <ul className={styles.navLinks}>
-          <li className={styles.link}><Link href="/">Home</Link></li>
-          <li className={styles.link}><Link href="/Projects">Proyects</Link></li>
-          <li className={styles.link}><Link href="/About">About</Link></li>
-        </ul>
+      <ul className={styles.navLinks}>
+        <li className={styles.link}><Link href="/">Home</Link></li>
+        <li className={styles.link}><Link href="/Projects">Proyects</Link></li>
+        <li className={styles.link}><Link href="/About">About</Link></li>
+      </ul>
 
-        <button className={styles.btn}>Curriculum</button>
+      <button className={styles.btn} onProgress={descargarPDF}>Curriculum</button>
 
-      </nav>
+    </nav>
   );
 };
 
